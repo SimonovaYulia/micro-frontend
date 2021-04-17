14e995af7b4e4d8622bef07a621928a9a51441fe
Iuliia Simonova <iuliia.sim@ascendcorp.com>
2021-04-17 18:30:07 +0700

    ---

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <h1>Container new test</h1>
+      <p>Container</p>
       <hr />
       <MarketingApp />
     </div>
