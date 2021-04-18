868066cd2880071de631e638a8b3c28dd424392a
Iuliia Simonova <iuliia.sim@ascendcorp.com>
2021-04-18 16:13:06 +0700

    ---

--- container/src/App.js
+++ container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from "./components/MarketingApp";
 export default () => {
   return (
     <div>
-      <p>MFE container</p>
+      <p>Main container</p>
       <hr />
       <MarketingApp />
     </div>
